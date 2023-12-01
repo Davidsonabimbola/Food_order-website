720ef82b1ea364a7c1efce00fe81fae40a9dbf27
Davidson Abimbola <108395763+Davidsonabimbola@users.noreply.github.com>
2023-12-01 03:13:06 +0200

    the hero text adjusted

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
         <div className='max-h-[500px] relative' >
 {/* overlay */}
             <div className=' absolute w-full h-full text-gray-200 max-h-[500px] bg-black/40 
-            flex flex-col justify-center translate-x-[-100px] md:ml-1 sm: ml-8'>
+            flex flex-col justify-center translate-x-[-100px] md:ml-1 sm: ml-12'>
                 <h1 className=' px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold'>The <span className=' text-orange-400'>Best</span></h1>
                 <h1 className=' px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold'>Food <span className=' text-orange-400'>Delivered</span></h1>
             </div>
